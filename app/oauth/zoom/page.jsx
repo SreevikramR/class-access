bf0e5e605f113bb8d0fb3b1e5e6cd23c80103b39
Sreevikram R <sreevikram.r@gmail.com>
2024-06-14 21:57:49 +0530

    bug fix

--- app/oauth/zoom/page.jsx
+++ app/oauth/zoom/page.jsx
@@ -1,9 +1,11 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation';
 
 const page = () => {
-    const code = useSearchParams().get('code')
+
+    const searchParams = useSearchParams()
+    const code = searchParams.get('code')
 
     return (
         <div>User code: {code}</div>
