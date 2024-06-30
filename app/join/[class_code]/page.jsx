6604c462e0ca50412eb85f5feebd8cfbf8a01323
SreevikramR <sreevikram.r@gmail.com>
2024-06-30 16:11:09 +0530

    UI added for different states

--- app/join/[class_code]/page.jsx
+++ app/join/[class_code]/page.jsx
@@ -1,75 +1,178 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { supabaseClient } from '@/components/util_function/supabaseCilent';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
 export default function Component({ params: { class_code } }) {
-	const [credits, setCredits] = useState(1); // Replace 1 with your actual credit value
+	const [isLoggedIn, setIsLoggedIn] = useState(true);
+	const [isUnauthorized, setIsUnauthorized] = useState(false);
+	const [noAccount, setNoAccount] = useState(false);
+	const [credits, setCredits] = useState(2); // Replace 1 with your actual credit value
 	const [willPay, setWillPay] = useState(false);
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	useEffect(() => {
+		fetchUser()
+	}, [])
+
+	const handleLogin = () => {}
+	const handleGoogleLogin = () => {}
+
+	const fetchUser = async () => {
+		const user = await supabaseClient.auth.getUser();
+		if (user.data.user != null) {
+			console.log("logged in")
+		} else {
+			console.log("not logged in")
+		}
+	}
 
 	return (
 		<div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
 			<main>
-				<div className="w-full flex justify-center items-center">
-					{credits == 0 && (
-						<Card className="w-[36vw] border-2">
-							<div className="text-center">
-								<h1 className="font-bold text-foreground sm:text-2xl pt-6">Class Name</h1>
-								<h2><p className="text-muted-foreground">Teacher: John Doe</p></h2>
-							</div>
-							<div className="rounded-lg bg-white p-3 pt-0">
-								<div className="flex flex-col sm:flex-row"></div>
-								<div className="mt-6 rounded-lg bg-red-500 px-4 py-3 text-red-50">
-									<div className="flex items-center">
-										<TriangleAlertIcon className="mr-2 h-5 w-5" />
-										<p className="text-sm font-medium">
-											We see that you have not paid yet. Please pay to join the class.
-										</p>
+				{isLoggedIn && (
+					<>
+					{!isUnauthorized && (
+						<div className="w-full flex justify-center items-center">
+							{credits == 0 && (
+								<Card className="w-[36vw] border-2">
+									<div className="text-center">
+										<h1 className="font-bold text-foreground sm:text-2xl pt-6">Class Name</h1>
+										<h2><p className="text-muted-foreground">Teacher: John Doe</p></h2>
 									</div>
-								</div>
-							</div>
-						</Card>
-					)}
-					{credits == 1 && (
-						<Card className="w-[36vw] border-2">
-							<div className="text-center">
-								<h1 className="font-bold text-foreground sm:text-2xl pt-6">Class Name</h1>
-								<h2><p className="text-muted-foreground">Teacher: John Doe</p></h2>
-							</div>
-							<div className="rounded-lg bg-white p-3 pt-0">
-								<div className="flex flex-col sm:flex-row"></div>
-								<div className="mt-6 rounded-lg bg-red-500 px-4 py-3 text-red-50">
-									<div className="flex items-center">
-										<TriangleAlertIcon className="mr-2 h-5 w-5" />
-										<p className="text-sm font-medium">
-											Kindly Pay before your next class
-										</p>
-									</div>
-								</div>
-								<div className="mt-6 flex justify-between items-center">
-									<div className='p-1 py-2 flex flex-row justify-center'>
-										<input
-											type="checkbox"
-											id="willPay"
-											checked={willPay}
-											onChange={() => setWillPay(!willPay)}
-											className="mr-2 w-5 h-5 border-2 checked:accent-green-600"
-										/>
-										<label htmlFor="willPay" className="text-foreground text-md items-center">I will complete the payment</label>
-									</div>
-									{willPay && (
-										<div className="flex justify-end">
-											<Button className="w-full sm:w-auto bg-green-700 hover:bg-green-500">Join Class</Button>
+									<div className="rounded-lg bg-white p-3 pt-0">
+										<div className="flex flex-col sm:flex-row"></div>
+										<div className="mt-6 rounded-lg bg-red-500 px-4 py-3 text-red-50">
+											<div className="flex items-center">
+												<TriangleAlertIcon className="mr-2 h-5 w-5" />
+												<p className="text-sm font-medium">
+													We see that you have not paid yet. Please pay to join the class.
+												</p>
+											</div>
 										</div>
-									)}
+									</div>
+								</Card>
+							)}
+							{credits == 1 && (
+								<Card className="w-[36vw] border-2">
+									<div className="text-center">
+										<h1 className="font-bold text-foreground sm:text-2xl pt-6">Class Name</h1>
+										<h2><p className="text-muted-foreground">Teacher: John Doe</p></h2>
+									</div>
+									<div className="rounded-lg bg-white p-3 pt-0">
+										<div className="flex flex-col sm:flex-row"></div>
+										<div className="mt-6 rounded-lg bg-red-500 px-4 py-3 text-red-50">
+											<div className="flex items-center">
+												<TriangleAlertIcon className="mr-2 h-5 w-5" />
+												<p className="text-sm font-medium">
+													Kindly Pay before your next class
+												</p>
+											</div>
+										</div>
+										<div className="mt-6 flex justify-between items-center">
+											<div className='p-1 py-2 flex flex-row justify-center'>
+												<input
+													type="checkbox"
+													id="willPay"
+													checked={willPay}
+													onChange={() => setWillPay(!willPay)}
+													className="mr-2 w-5 h-5 border-2 checked:accent-green-600"
+													/>
+												<label htmlFor="willPay" className="text-foreground text-md items-center">I will complete the payment</label>
+											</div>
+											{willPay && (
+												<div className="flex justify-end">
+													<Button className="w-full sm:w-auto bg-green-700 hover:bg-green-500">Join Class</Button>
+												</div>
+											)}
+										</div>
+									</div>
+								</Card>
+							)}
+							{credits >= 2 && (
+								<div>Redirecting you to your class...</div>
+							)}
+						</div>
+					)}
+					{isUnauthorized && (
+							<Card className="w-[36vw] border-2 p-10">
+								<div className="text-center">
+									<h1 className="font-semibold text-lg text-foreground">You do not have access to this class. Please contact your instructor for more details</h1>
 								</div>
-							</div>
-						</Card>
-					)}
-					{credits >= 2 && (
-						<div>Redirecting you to your class...</div>
-					)}
-				</div>
+							</Card>
+							)}
+					</>
+				)}
+				{!isLoggedIn && (
+					<>
+						{!noAccount && (
+							<Card className="w-[36vw] border-2">
+								<div className="text-center">
+									<h1 className="font-semibold text-xl text-foreground pt-6">Please Login to Join your class</h1>
+								</div>
+								<div className="rounded-lg bg-white p-3 pt-0">
+									<div className="grid gap-4">
+										<div className="grid gap-2">
+											<Label htmlFor="email">Email</Label>
+											<Input
+												id="email"
+												type="email"
+												value={email}
+												placeholder="email@example.com"
+												required
+												onChange={(e) => setEmail(e.target.value)}
+											/>
+										</div>
+										<div className="grid gap-2">
+											<div className="flex items-center">
+												<Label htmlFor="password">Password</Label>
+												<Link
+													href="/forgot-password"
+													className="ml-auto inline-block text-sm underline"
+												>
+													Forgot your password?
+												</Link>
+											</div>
+											<Input
+												id="password"
+												placeholder="&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;"
+												type="password"
+												required
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</div>
+										<Button type="submit" onClick={handleLogin} className="w-full">
+											Login
+										</Button>
+										<div className="flex items-center my-2">
+											<hr className="flex-grow border-t border-gray-300" />
+											<span className="mx-2 text-gray-500 text-xs">OR CONTINUE WITH</span>
+											<hr className="flex-grow border-t border-gray-300" />
+										</div>
+										<Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+											Google
+										</Button>
+										<div className='text-md cursor-pointer text-blue-700 underline w-fit' onClick={() => setNoAccount(true)}>Don&apos;t have an Account?</div>	
+									</div>
+								</div>
+							</Card>
+						)}
+						{noAccount && (
+							<Card className="w-[36vw] border-2 p-10">
+								<div className="text-center">
+									<h1 className="font-semibold text-lg text-foreground">Please contact your instructor to gain access to the class</h1>
+								</div>
+							</Card>
+						)}
+					</>
+				)}
 			</main>
 		</div>
 	);
