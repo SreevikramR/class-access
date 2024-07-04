d131549739020fa31d2d3cfd201d4014d065e9ef
SreevikramR <sreevikram.r@gmail.com>
2024-07-04 19:03:41 +0530

    automatically refresh tokens and create meeting

--- app/api/zoom/create_meeting/route.js
+++ app/api/zoom/create_meeting/route.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-const zoomAuthUrl = 'https://zoom.us/oauth/token';
 const zoomMeetingUrl = 'https://api.zoom.us/v2/users/me/meetings';
 const retrieve_tokens_url = new URL('https://classaccess.tech/api/zoom/retrieve_tokens');
 
 export async function POST(request) {
-	console.log(request.headers)
-
+	let refresh_token = ""
 	try {
 		// const accessToken = "eyJzdiI6IjAwMDAwMSIsImFsZyI6IkhTNTEyIiwidiI6IjIuMCIsImtpZCI6IjZhNzM2NWNiLTk1ZDYtNGEyNi1hZmYyLTVjNTZlMDA0MmI5NSJ9.eyJ2ZXIiOjksImF1aWQiOiI0ODEyNzk1YzY3NWViYjE0OWNhZTZlNjJhN2EwZDkzZiIsImNvZGUiOiJwVFBkN0FQOUVaNFVoZVdSVG9XUjFpZkVmM1Q4ZzdtRlEiLCJpc3MiOiJ6bTpjaWQ6dEpzbWRhbHdTN2lpeG5IYm5naHNydyIsImdubyI6MCwidHlwZSI6MCwidGlkIjowLCJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJMQmxYR3B5OFFueVVWOGh0YVBYcDR3IiwibmJmIjoxNzE5MjM0NDU2LCJleHAiOjE3MTkyMzgwNTYsImlhdCI6MTcxOTIzNDQ1NiwiYWlkIjoiTXhDSHlkTTlRSENnZ2I2MWNDblVqQSJ9.inDp5Aw_5Qw9_vi8Hbu4aN-Kz-MOrH3HWqvpsEoSk7RmhwtSU5TdUFQjNVNdTVBU4CLHR3QtFmELsUgu1IEKPQ";
 		const jwt = request.headers.get('jwt');
@@ -16,9 +14,10 @@ export async function POST(request) {
 		const tokenData = await axios.get(retrieve_tokens_url,
 			{ headers: { 'jwt': jwt, 'supabase_refresh': supabase_refresh } }
 		)
-		console.log(tokenData)
-		const accessToken = tokenData.data[0].access_token
+		let accessToken = tokenData.data[0].access_token
+		refresh_token = tokenData.data[0].refresh_token
 
+		
 		const meetingResponse = await axios.post(
 			zoomMeetingUrl,
 			{
@@ -36,7 +35,9 @@ export async function POST(request) {
 		const meetingLink = meetingResponse.data.join_url;
 		return NextResponse.json({ meetingLink }, { status: 200 });
 	} catch (error) {
-		console.error(error);
+		if (error.response.data.code === 124) {
+			return NextResponse.json({ error: 'Invalid access token', refresh_token: refresh_token }, { status: 401 });
+		}
 		return NextResponse.json({ error }, { status: 500 });
 	}
 }
