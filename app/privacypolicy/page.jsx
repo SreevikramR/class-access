5b62164fcdb3dd5a9cc9ec7786076c5c9b65f517
SreevikramR <sreevikram.r@gmail.com>
2024-07-06 23:10:17 +0530

    Added TOS and Privacy Policy

---
+++ app/privacypolicy/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>https://docs.google.com/document/d/1G1jyBFYv6z8Bl6BMmvs-Eebx4iOpXNkvXV9tVoOQKMU/edit</div>
+    )
+}
+
+export default Page
