6d7cfeaa5edc6c397f3877635be1dbc976de069a
SreevikramR <sreevikram.r@gmail.com>
2024-07-15 10:44:29 +0530

    new_student api new account path added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,83 +1,82 @@
-"use client"
-import React, { useState, useEffect } from 'react'
-import { supabaseClient } from '@/components/util_function/supabaseCilent'
-import Header from '@/components/page_components/header'
-import Footer from '@/components/page_components/footer'
-import { Button } from '@/components/ui/button'
-import { PlusCircle, Share2, Copy } from 'lucide-react';
-import CreateClassPopup from '@/components/page_components/Dialogs/createClassPopup'
-import AuthWrapper from '@/components/page_components/authWrapper'
-import LoadingOverlay from '@/components/page_components/loadingOverlay'
-import { useRouter } from 'next/navigation';
+import React, { useState, useEffect } from "react";
+import { supabaseClient } from "@/components/util_function/supabaseCilent";
+import Header from "@/components/page_components/header";
+import Footer from "@/components/page_components/footer";
+import { Button } from "@/components/ui/button";
+import { PlusCircle, Share2, Copy } from "lucide-react";
+import CreateClassPopup from "@/components/page_components/Dialogs/createClassPopup";
+import AuthWrapper from "@/components/page_components/authWrapper";
+import LoadingOverlay from "@/components/page_components/loadingOverlay";
+import { useRouter } from "next/navigation";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
-import { Input } from '@/components/ui/input';
+import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
 
 const convertTo12HourFormat = (time) => {
-    const [hours, minutes] = time.split(':');
-    const period = hours >= 12 ? 'PM' : 'AM';
-    const adjustedHours = hours % 12 || 12; // Convert '0' to '12'
-    return `${adjustedHours}:${minutes} ${period}`;
+	const [hours, minutes] = time.split(":");
+	const period = hours >= 12 ? "PM" : "AM";
+	const adjustedHours = hours % 12 || 12; // Convert '0' to '12'
+	return `${adjustedHours}:${minutes} ${period}`;
 };
 
 const Dashboard = ({ classInfo }) => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [classes, setClasses] = useState([])
-    const [loading, setLoading] = useState(false)
-    const router = useRouter()
-    const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
-    const [copyData, setCopyData] = useState(null)
+	const [isOpen, setIsOpen] = useState(false);
+	const [classes, setClasses] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const router = useRouter();
+	const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
+	const [copyData, setCopyData] = useState(null);
 
-    useEffect(() => {
-        fetchClasses()
-    }, [])
+	useEffect(() => {
+		fetchClasses();
+	}, []);
 
-    useEffect(() => {
-        if (!isOpen) {
-            fetchClasses();
-        }
-    }, [isOpen]);
+	useEffect(() => {
+		if (!isOpen) {
+			fetchClasses();
+		}
+	}, [isOpen]);
 
-    const fetchClasses = async () => {
-        setLoading(true)
-        const { data, error } = await supabaseClient
-            .from('classes')
-            .select('*')
-            .eq('teacher_id', (await supabaseClient.auth.getUser()).data.user.id)
+	const fetchClasses = async () => {
+		setLoading(true);
+		const { data, error } = await supabaseClient
+			.from("classes")
+			.select("*")
+			.eq("teacher_id", (await supabaseClient.auth.getUser()).data.user.id);
 
-        if (error) {
-            console.error('Error fetching classes:', error)
-        } else {
-            setClasses(data)
-        }
-        console.log(data)
-        setLoading(false)
-    }
+		if (error) {
+			console.error("Error fetching classes:", error);
+		} else {
+			setClasses(data);
+		}
+		console.log(data);
+		setLoading(false);
+	};
 
-    const handleCopyLink = () => {
-        const shareLink = `${window.location.origin}/join/${copyData.class_code}`;
-        navigator.clipboard.writeText(copyData.classLink).then(() => {
-            toast({
-                title: "Link copied!",
-                description: "The class link has been copied to your clipboard.",
-            });
-        });
-        setIsShareDialogOpen(false);
-    };
+	const handleCopyLink = () => {
+		const shareLink = `${window.location.origin}/join/${copyData.class_code}`;
+		navigator.clipboard.writeText(copyData.classLink).then(() => {
+			toast({
+				title: "Link copied!",
+				description: "The class link has been copied to your clipboard.",
+			});
+		});
+		setIsShareDialogOpen(false);
+	};
 
-    const handleShareClick = (classData) => {
-        console.log(classData)
-        setCopyData(classData);
-        setIsShareDialogOpen(true)
-    }
+	const handleShareClick = (classData) => {
+		console.log(classData);
+		setCopyData(classData);
+		setIsShareDialogOpen(true);
+	};
 
-    const _classCard = (classInfo) => {
-        const classCode = classInfo.class_code
-        const classLink = `${window.location.origin}/join/${classCode}`
+	const _classCard = (classInfo) => {
+		const classCode = classInfo.class_code;
+		const classLink = `${window.location.origin}/join/${classCode}`;
 
-        let classData = {}
-        classData.classCode = classCode
-        classData.classLink = classLink
+		let classData = {};
+		classData.classCode = classCode;
+		classData.classLink = classLink;
 
         const handleStartClass = () => {
             if (classInfo.meeting_link) {
@@ -132,59 +131,80 @@ const Dashboard = ({ classInfo }) => {
         )
     }
 
-    return (
-        <AuthWrapper>
-            {loading && <LoadingOverlay />}
-            <div className="flex flex-col min-h-screen">
-                <Header />
-                <Dialog open={isShareDialogOpen} onOpenChange={setIsShareDialogOpen} className="bg-white">
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>Share Class Link</DialogTitle>
-                            <DialogDescription>Copy the link below to share this class</DialogDescription>
-                        </DialogHeader>
-                        <div className="flex items-center space-x-2">
-                            <Input
-                                readOnly
-                                value={copyData?.classLink}
-                            />
-                            <Button onClick={handleCopyLink} size="sm">
-                                <Copy className="h-4 w-4 mr-2" />
-                                Copy
-                            </Button>
-                        </div>
-                    </DialogContent>
-                </Dialog>
-                <main className="flex-1 bg-gray-100 px-4 py-8 sm:px-6 grid gap-8">
-                    <section>
-                        <CreateClassPopup isOpen={isOpen} setIsOpen={setIsOpen} />
-                        <div className="flex items-center justify-between my-2">
-                            <h2 className="text-2xl font-semibold px-2">My Classes</h2>
-                            <Button size="sm" className="h-7 gap-1 hover:bg-zinc-700">
-                                <PlusCircle className="h-3.5 w-3.5" />
-                                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap" onClick={() => setIsOpen(true)}>
-                                    Add Class
-                                </span>
-                            </Button>
-                        </div>
-                        <div className="grid gap-4">
-                            {classes.length === 0 &&
-                                <>
-                                    {loading && <div className='flex flex-col bg-white border-2 m-2 rounded-xl w-full h-[65vh] items-center justify-center'>Loading...</div>}
-                                    {!loading && <div className='flex flex-col bg-white border-2 m-2 rounded-xl w-full h-[65vh] items-center justify-center'>
-                                        <div className='text-lg mb-3'>No Classes Found!</div>
-                                        <div className='flex flex-row items-center text-muted-foreground'>Would you like to add a new class?</div>
-                                        <div className='flex flex-row ml-2 hover:bg-black hover:text-white items-center border-2 border-black hover:cursor-pointer p-1 px-2 rounded-lg mt-2' onClick={() => setIsOpen(true)}><PlusCircle className='h-4 w-4 mr-1' />Add Class</div>
-                                    </div>}
-                                </>}
-                            {classes.map((classInfo) => _classCard(classInfo))}
-                        </div>
-                    </section>
-                </main>
-                <Footer />
-            </div>
-        </AuthWrapper>
-    )
-}
+	return (
+		<AuthWrapper>
+			{loading && <LoadingOverlay />}
+			<div className="flex flex-col min-h-screen">
+				<Header />
+				<Dialog
+					open={isShareDialogOpen}
+					onOpenChange={setIsShareDialogOpen}
+					className="bg-white"
+				>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Share Class Link</DialogTitle>
+							<DialogDescription>
+								Copy the link below to share this class
+							</DialogDescription>
+						</DialogHeader>
+						<div className="flex items-center space-x-2">
+							<Input readOnly value={copyData?.classLink} />
+							<Button onClick={handleCopyLink} size="sm">
+								<Copy className="h-4 w-4 mr-2" />
+								Copy
+							</Button>
+						</div>
+					</DialogContent>
+				</Dialog>
+				<main className="flex-1 bg-gray-100 px-4 py-8 sm:px-6 grid gap-8">
+					<section>
+						<CreateClassPopup isOpen={isOpen} setIsOpen={setIsOpen} />
+						<div className="flex items-center justify-between my-2">
+							<h2 className="text-2xl font-semibold px-2">My Classes</h2>
+							<Button size="sm" className="h-7 gap-1 hover:bg-zinc-700">
+								<PlusCircle className="h-3.5 w-3.5" />
+								<span
+									className="sr-only sm:not-sr-only sm:whitespace-nowrap"
+									onClick={() => setIsOpen(true)}
+								>
+									Add Class
+								</span>
+							</Button>
+						</div>
+						<div className="grid gap-4">
+							{classes.length === 0 && (
+								<>
+									{loading && (
+										<div className="flex flex-col bg-white border-2 m-2 rounded-xl w-full h-[65vh] items-center justify-center">
+											Loading...
+										</div>
+									)}
+									{!loading && (
+										<div className="flex flex-col bg-white border-2 m-2 rounded-xl w-full h-[65vh] items-center justify-center">
+											<div className="text-lg mb-3">No Classes Found!</div>
+											<div className="flex flex-row items-center text-muted-foreground">
+												Would you like to add a new class?
+											</div>
+											<div
+												className="flex flex-row ml-2 hover:bg-black hover:text-white items-center border-2 border-black hover:cursor-pointer p-1 px-2 rounded-lg mt-2"
+												onClick={() => setIsOpen(true)}
+											>
+												<PlusCircle className="h-4 w-4 mr-1" />
+												Add Class
+											</div>
+										</div>
+									)}
+								</>
+							)}
+							{classes.map((classInfo) => _classCard(classInfo))}
+						</div>
+					</section>
+				</main>
+				<Footer />
+			</div>
+		</AuthWrapper>
+	);
+};
 
 export default Dashboard;
