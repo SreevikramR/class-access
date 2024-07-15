59d39ad7387e31b4f46e627433ad9acfc4911a6d
Meet Gamdha <gamdhameet@gmail.com>
2024-07-15 19:01:00 +0530

    api

--- components/page_components/Dialogs/createClassPopup.jsx
+++ components/page_components/Dialogs/createClassPopup.jsx
@@ -107,7 +107,7 @@ const CreateClassPopup = ({isOpen, setIsOpen}) => {
 			for (const student of tempNewStudents) {
 				
 				const jwt = (await supabaseClient.auth.getSession()).data.session.access_token;
-				const response = await fetchTimeout(`/api/users/new_student`, 10000, {
+				const response = await fetchTimeout(`/api/students/new_student`, 10000, {
 					method: 'POST',
 					headers: {
 						"jwt": jwt,
