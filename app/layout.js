c08fd5902dc672f95623932a99e0b4708f05969b
SreevikramR <sreevikram.r@gmail.com>
2024-07-20 08:04:35 +0530

    completed forgot password + added vercel speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <head>
         <script dangerouslySetInnerHTML={{
           __html: `
