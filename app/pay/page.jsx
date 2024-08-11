0ef49ab664aba139095b296dcda8b75ee9438146
SreevikramR <sreevikram.r@gmail.com>
2024-08-11 18:12:28 -0500

    added suspense

--- app/pay/page.jsx
+++ app/pay/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 import LoadingOverlay from '@/components/page_components/loadingOverlay';
 import { Card, CardHeader, CardContent } from "@/components/ui/card"
 import { Label } from '@/components/ui/label';
@@ -163,50 +163,51 @@ const Payments = () => {
 
 	return (
 		<div className="flex flex-wrap flex-col justify-center h-screen items-center">
-			{loading && <LoadingOverlay />}
-			{!loggedIn && (
-				<>
-					{!noAccount && (
-						<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 h-fit">
-							<div className="text-center">
-								<h1 className="font-semibold text-md pb-4 lg:text-xl text-foreground pt-6 text-pretty">Please Login to Make Payments</h1>
-							</div>
-							<div className="rounded-lg bg-white p-3 pt-0">
-								<div className="grid gap-4">
-									<div className="grid gap-2">
-										<Label htmlFor="email">Email</Label>
-										<Input
-											id="email"
-											type="email"
-											value={email}
-											placeholder="email@example.com"
-											required
-											onChange={(e) => setEmail(e.target.value)}
-										/>
-									</div>
-									<div className="grid gap-2">
-										<div className="flex items-center">
-											<Label htmlFor="password">Password</Label>
-											<Link
-												href="/forgot-password"
-												className="ml-auto inline-block text-xs sm:text-sm underline"
-											>
-													Forgot your password?
-											</Link>
+			<Suspense fallback={<LoadingOverlay />}>
+				{loading && <LoadingOverlay />}
+				{!loggedIn && (
+					<>
+						{!noAccount && (
+							<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 h-fit">
+								<div className="text-center">
+									<h1 className="font-semibold text-md pb-4 lg:text-xl text-foreground pt-6 text-pretty">Please Login to Make Payments</h1>
+								</div>
+								<div className="rounded-lg bg-white p-3 pt-0">
+									<div className="grid gap-4">
+										<div className="grid gap-2">
+											<Label htmlFor="email">Email</Label>
+											<Input
+												id="email"
+												type="email"
+												value={email}
+												placeholder="email@example.com"
+												required
+												onChange={(e) => setEmail(e.target.value)}
+											/>
 										</div>
-										<Input
-											id="password"
-											placeholder="&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;"
-											type="password"
-											required
-											value={password}
-											onChange={(e) => setPassword(e.target.value)}
-										/>
-									</div>
-									<Button type="submit" onClick={handleLogin} className="w-full">
+										<div className="grid gap-2">
+											<div className="flex items-center">
+												<Label htmlFor="password">Password</Label>
+												<Link
+													href="/forgot-password"
+													className="ml-auto inline-block text-xs sm:text-sm underline"
+												>
+													Forgot your password?
+												</Link>
+											</div>
+											<Input
+												id="password"
+												placeholder="&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;"
+												type="password"
+												required
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</div>
+										<Button type="submit" onClick={handleLogin} className="w-full">
 											Login
-									</Button>
-									{/* <div className="flex items-center my-2">
+										</Button>
+										{/* <div className="flex items-center my-2">
 											<hr className="flex-grow border-t border-gray-300" />
 											<span className="mx-2 text-gray-500 text-xs">OR CONTINUE WITH</span>
 											<hr className="flex-grow border-t border-gray-300" />
@@ -214,80 +215,81 @@ const Payments = () => {
 										<Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
 											Google
 										</Button> */}
-									<div className='sm:text-md text-sm cursor-pointer text-blue-700 underline w-fit' onClick={() => setNoAccount(true)}>Don&apos;t have an Account?</div>
+										<div className='sm:text-md text-sm cursor-pointer text-blue-700 underline w-fit' onClick={() => setNoAccount(true)}>Don&apos;t have an Account?</div>
+									</div>
 								</div>
-							</div>
-						</Card>
-					)}
-					{noAccount && (
-						<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
-							<div className="text-center">
-								<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Please contact your instructor to gain access to the class and payment options</h1>
-							</div>
-						</Card>
-					)}
-				</>
-			)}
-			{loggedIn && noInvoiceFound && (
-				<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
-					<div className="text-center">
-						<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">No Invoice Found</h1>
-						<p className="text-sm text-gray-500">Please contact your instructor for more information</p>
-					</div>
-				</Card>
-			)}
-			{loggedIn && !noInvoiceFound && noUPI && (
-				<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
-					<div className="text-center">
-						<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Unable to Make Payment</h1>
-						<p className="text-sm text-gray-500 pt-2">Your instructor has not configured UPI payments. Please contact them for more information on how to pay</p>
-					</div>
-				</Card>
-			)}
-			{loggedIn && !noInvoiceFound && invoice !== null && classDetails !== null && teacherDetails !== null && !hasPaid && !noUPI && (
-				<>
-					<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 border-black h-fit">
-						<CardHeader className="bg-primary text-primary-foreground p-6 flex justify-between items-center text-center">
-							<div className="space-y-1">
-								<div className="font-semibold">{classDetails.name}</div>
-								<div>{teacherDetails.first_name} {teacherDetails.last_name}</div>
-							</div>
-							<div className="text-4xl font-bold">&#8377;{invoice.amount}</div>
-						</CardHeader>
-						<CardContent className="p-6 grid gap-4">
-							<div className="flex justify-between">
-								<div className="text-muted-foreground">Invoice Date</div>
-								<div>{date}</div>
-							</div>
-							<Separator />
-							<div className="flex justify-center">
-								<Link href={link} className="block" prefetch={false}>
-									<QRCodeSVG value={link}/>
-								</Link>
-							</div>
-							<div className="text-center text-muted-foreground">Scan or click the QR code to make a UPI payment</div>
-						</CardContent>
+							</Card>
+						)}
+						{noAccount && (
+							<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
+								<div className="text-center">
+									<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Please contact your instructor to gain access to the class and payment options</h1>
+								</div>
+							</Card>
+						)}
+					</>
+				)}
+				{loggedIn && noInvoiceFound && (
+					<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
+						<div className="text-center">
+							<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">No Invoice Found</h1>
+							<p className="text-sm text-gray-500">Please contact your instructor for more information</p>
+						</div>
 					</Card>
-					<Button className="bg-green-600 mt-6 hover:bg-green-700" onClick={handlePaid} disabled={!paymentConfirmActive}>I Have Paid!</Button>
-					<div className="text-center text-muted-foreground pt-2">After paying, return to this link and click on the button above</div>
-				</>
-			)}
-			{loggedIn && hasPaid && !noUPI && !hasConfirmed && (
-				<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
-					<div className="text-center">
-						<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Payment Complete!</h1>
-						<p className="text-sm text-gray-500 pt-2">Thank you for paying! We will email you a confirmation once your instructor confirms receiving the payment.</p>
-					</div>
-				</Card>
-			)}
-			{loggedIn && hasPaid && hasConfirmed && !noUPI && (
-				<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
-					<div className="text-center">
-						<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Payment Complete!</h1>
-						<p className="text-sm text-gray-500 pt-2">Your payment has been confirmed by your instructor. Please look in your email for the confirmation</p>
-					</div>
-				</Card>
-			)}
+				)}
+				{loggedIn && !noInvoiceFound && noUPI && (
+					<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
+						<div className="text-center">
+							<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Unable to Make Payment</h1>
+							<p className="text-sm text-gray-500 pt-2">Your instructor has not configured UPI payments. Please contact them for more information on how to pay</p>
+						</div>
+					</Card>
+				)}
+				{loggedIn && !noInvoiceFound && invoice !== null && classDetails !== null && teacherDetails !== null && !hasPaid && !noUPI && (
+					<>
+						<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 border-black h-fit">
+							<CardHeader className="bg-primary text-primary-foreground p-6 flex justify-between items-center text-center">
+								<div className="space-y-1">
+									<div className="font-semibold">{classDetails.name}</div>
+									<div>{teacherDetails.first_name} {teacherDetails.last_name}</div>
+								</div>
+								<div className="text-4xl font-bold">&#8377;{invoice.amount}</div>
+							</CardHeader>
+							<CardContent className="p-6 grid gap-4">
+								<div className="flex justify-between">
+									<div className="text-muted-foreground">Invoice Date</div>
+									<div>{date}</div>
+								</div>
+								<Separator />
+								<div className="flex justify-center">
+									<Link href={link} className="block" prefetch={false}>
+										<QRCodeSVG value={link}/>
+									</Link>
+								</div>
+								<div className="text-center text-muted-foreground">Scan or click the QR code to make a UPI payment</div>
+							</CardContent>
+						</Card>
+						<Button className="bg-green-600 mt-6 hover:bg-green-700" onClick={handlePaid} disabled={!paymentConfirmActive}>I Have Paid!</Button>
+						<div className="text-center text-muted-foreground pt-2">After paying, return to this link and click on the button above</div>
+					</>
+				)}
+				{loggedIn && hasPaid && !noUPI && !hasConfirmed && (
+					<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
+						<div className="text-center">
+							<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Payment Complete!</h1>
+							<p className="text-sm text-gray-500 pt-2">Thank you for paying! We will email you a confirmation once your instructor confirms receiving the payment.</p>
+						</div>
+					</Card>
+				)}
+				{loggedIn && hasPaid && hasConfirmed && !noUPI && (
+					<Card className="lg:w-[36vw] sm:w-[60vw] w-[90vw] border-2 p-10 h-fit">
+						<div className="text-center">
+							<h1 className="font-semibold text-md sm:text-lg text-foreground text-pretty">Payment Complete!</h1>
+							<p className="text-sm text-gray-500 pt-2">Your payment has been confirmed by your instructor. Please look in your email for the confirmation</p>
+						</div>
+					</Card>
+				)}
+			</Suspense>
 		</div>
 	)
 }
