37e3fd8ffc6152ab90fa696abd29d408b4661dad
SreevikramR <sreevikram.r@gmail.com>
2024-08-17 16:28:14 -0500

    changed from google meet api to googleapis library

--- app/api/meetings/create/gmeet/route.js
+++ app/api/meetings/create/gmeet/route.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 import fetchTimeout from '@/components/util_function/fetch';
 import verifyJWT from '@/components/util_function/verifyJWT';
-import { SpacesServiceClient } from '@google-apps/meet';
+import { google } from 'googleapis';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
@@ -12,8 +12,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function POST(request) {
 	const jwt = request.headers.get('jwt');
-
-	const decodedJWT = verifyJWT(token);
+	const decodedJWT = verifyJWT(jwt);
 	const teacherUUID = decodedJWT?.sub;
 	if (!decodedJWT) {
 		return NextResponse.json({message: "Invalid Token"}, {status: 401});
@@ -24,6 +23,33 @@ export async function POST(request) {
 		return NextResponse.json({ error: 'Server Error' }, { status: 500 });
 	} else if (data.length == 0) {
 		return NextResponse.json({ error: 'Teacher not found' }, { status: 500 });
+	}
+
+	const oauth2client = new google.auth.OAuth2('clientID', 'clientSecret', 'redirectURI')
+	oauth2client.setCredentials({access_token: 'accessToken', refresh_token: 'refreshToken'})
+	const meet = google.calendar({version: 'v3', auth: oauth2client});
+	const event = {
+		summary: 'Meeting',
+		location: 'Online',
+		description: 'Meeting with student',
+		start: {
+			dateTime: '2022-01-01T09:00:00-07:00',
+			timeZone: 'Asia/Kolkata',
+		},
+		end: {
+			dateTime: '2022-01-01T10:00:00-07:00',
+			timeZone: 'Asia/Kolkata',
+		}
+	}
+
+	try {
+		const response = await meet.events.insert({
+			calendarId: 'primary',
+			resource: event
+		})
+		console.log(response.data)
+	} catch {
+		console.log("Error")
 	}
 
 
